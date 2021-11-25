8b385f0089007997326614ac71f4ba6fffe842f0
Tan Chau <cttan2000@gmail.com>
2021-11-25 22:41:09 +0700

    Update product list

--- view/client/product/list.php
+++ view/client/product/list.php
@@ -131,10 +131,10 @@
                                             </span></li>
                                         <li class="pull-right">
                                             <ul class="list-inline product-ratings">
-                                                <li><i class="rating<?php if ($product->rate > 1) echo '-selected';?> fa fa-star"></i></li>
-                                                <li><i class="rating<?php if ($product->rate > 2) echo '-selected';?> fa fa-star"></i></li>
-                                                <li><i class="rating<?php if ($product->rate > 3) echo '-selected';?> fa fa-star"></i></li>
-                                                <li><i class="rating<?php if ($product->rate > 4) echo '-selected';?> fa fa-star"></i></li>
+                                                <li><i class="rating<?php if ($product->rate >= 1) echo '-selected';?> fa fa-star"></i></li>
+                                                <li><i class="rating<?php if ($product->rate >= 2) echo '-selected';?> fa fa-star"></i></li>
+                                                <li><i class="rating<?php if ($product->rate >= 3) echo '-selected';?> fa fa-star"></i></li>
+                                                <li><i class="rating<?php if ($product->rate >= 4) echo '-selected';?> fa fa-star"></i></li>
                                                 <li><i class="rating<?php if ($product->rate == 5) echo '-selected';?> fa fa-star"></i></li>
                                             </ul>
                                             <?php if ($product->rate == 0) echo 'Chưa đánh giá';?>
